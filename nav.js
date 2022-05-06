b701bf5781b38ba0e631777ccbce6847bb78e387
Liisagrant <lisagrant1990@gmail.com>
2022-05-06 21:19:23 +0200

    forgot to push
    Have done small changes in footer.
    Removed logo. Need more work on it.
    fixed the hamburge menu icon from "menu" to standar lines.

--- nav.js
+++ nav.js
@@ -2,12 +2,13 @@ const mobielMenuBtn = document.querySelector ("#mobile-menu-btn");
 const mobileMenu = document.querySelector(".mobile-menu");
 
 mobielMenuBtn.addEventListener("click", () => {
+
     if (mobileMenu.style.display === "none") {
         mobileMenu.style.display ="flex";
-        mobielMenuBtn.innerHTML = "Close";
     }
     else {
         mobileMenu.style.display = "none";
-        mobielMenuBtn.innerHTML = "Menu";
+
     }
 });
+
