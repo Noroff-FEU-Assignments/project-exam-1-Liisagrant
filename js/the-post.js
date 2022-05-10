8ccc0cf1874e288985a6ffc3e70f6a3be3bc19a0
Liisagrant <lisagrant1990@gmail.com>
2022-05-10 20:35:05 +0200

    modal up
     Got the blog post up and going. Need small changes but works on all screens. Will tidy up the cods and fix some styling

--- js/the-post.js
+++ js/the-post.js
@@ -1,1 +1,24 @@
+const modal = document.querySelector(".modal");
+const image = document.querySelectorAll(".the-post-box-images-grid img");
+const orgianl  = document.querySelector(".full-img");
+const imageText = document.querySelector("caption");
 
+
+image.forEach(image => {
+    image.addEventListener("click", () => {
+        modal.classList.add("open");
+        orgianl.classList.add("open");
+
+        const orginalSrc = image.getAttribute("data-orginal");
+        orgianl.src= 
+        `./image/${orginalSrc}`
+    })
+})
+
+
+modal.addEventListener("click", (e) => {
+    if(e.target.classList.contains("modal")) {
+        modal.classList.remove("open");
+        orgianl.classList.remove("open");
+    }
+})
