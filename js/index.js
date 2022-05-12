16f37954e5079e1a52294225f9bd679b42599a06
Liisagrant <lisagrant1990@gmail.com>
2022-05-12 20:10:45 +0200

    started on the api call.
    Got the title and img, but need to figure out how to get the short text out. also have no idea where to find the other imgs.

--- js/index.js
+++ js/index.js
@@ -1,20 +1,32 @@
 const url = "https://lisagrant-943890.ingress-baronn.easywp.com/wp-json/wp/v2/posts?_embed=true";
-const BlogPostContainer = document.querySelector(".blog-post-card");
+const blogPostContainer = document.querySelector(".blog-post-card");
 
 
 const getBlogPost = async () => {
     try{
         const response = await fetch(url);
-        const blogPostInfo = await response.json();
-        console.log(blogPostInfo);
-
-        
+        const blogPosts = await response.json();
+        console.log(blogPosts);
+        for (blog of blogPosts) {
+            let images = blog._embedded["wp:featuredmedia"]
+            for (img of images){
+                console.log(img)
+                blogPostContainer.innerHTML += `
+                    <div class="blog-post-image-box">
+                        <a href="the-post.html?id=${blog.id}">
+                             <img src="${img.source_url}" alt=""/>
+                             <div class="text-box-carousel">
+                                <h3>${blog.title.rendered}</h3>
+                                <p>${blog.excerpt.rendered}</p>
+                            </div>
+                     </div>
+                `
+            }
+        }
     }
     catch(error){
-        
-
+        blogPostContainer.innerHTML = `Sorry we have an error`
     }
-
 }   
 
 getBlogPost()
