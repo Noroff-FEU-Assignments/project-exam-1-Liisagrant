09d634dc067b03264e158e23122bea33690eb535
Liisagrant <lisagrant1990@gmail.com>
2022-05-13 15:46:00 +0200

    Small changes in html and started on the post js
    finally got the right post . changed some styling. been thinking about the carousel. trying to figure out how to woke with it. also tidy up in some htm land css

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 const url = "https://lisagrant-943890.ingress-baronn.easywp.com/wp-json/wp/v2/posts?_embed=true";
 const blogPostContainer = document.querySelector(".blog-post-card");
+const backBtn = document.querySelector("#btn-back");
+const nextBtn = document.querySelector("#btn-back");
+
 
 
 const getBlogPost = async () => {
@@ -7,26 +10,31 @@ const getBlogPost = async () => {
         const response = await fetch(url);
         const blogPosts = await response.json();
         console.log(blogPosts);
-        for (blog of blogPosts) {
-            let images = blog._embedded["wp:featuredmedia"]
-            for (img of images){
-                console.log(img)
-                blogPostContainer.innerHTML += `
-                    <div class="blog-post-image-box">
-                        <a href="the-post.html?id=${blog.id}">
-                             <img src="${img.source_url}" alt=""/>
-                             <div class="text-box-carousel">
-                                <h3>${blog.title.rendered}</h3>
-                                <p>${blog.excerpt.rendered}</p>
-                            </div>
-                     </div>
-                `
+        for (blog of blogPosts){
+            let data = blog._embedded["wp:featuredmedia"]
+            for (img of data){
+                let newPost = `
+            <a href="the-post.html?id=${blog.id}">
+                <div class="blog-post-image-box">
+                         <img src="${img.source_url}" alt=""/>
+                        <div class="text-box-carousel">
+                            <h3>${blog.title.rendered}</h3>
+                            <p>${blog.excerpt.rendered}</p>
+                        </div>
+                </div>
+            </a>
+            `;
+            blogPostContainer.innerHTML = newPost;
+
+            }
             }
         }
-    }
     catch(error){
         blogPostContainer.innerHTML = `Sorry we have an error`
-    }
-}   
+    }  
+} 
 
-getBlogPost()
+getBlogPost();
+
+
+
--- js/the-post.js
+++ js/the-post.js
@@ -1,8 +1,63 @@
+const query = document.location.search;
+const param = new URLSearchParams(query);
+const id = param.get("id");
+
 const modal = document.querySelector(".modal");
 const image = document.querySelectorAll(".the-post-box-images-grid img");
 const orgianl  = document.querySelector(".full-img");
 const imageText = document.querySelector("caption");
 
+const apiUrl = `https://lisagrant-943890.ingress-baronn.easywp.com/wp-json/wp/v2/posts/${id}?_embed=true`
+const postBox = document.querySelector(".the-post-box");
+
+const getPost = async () => {
+    try{
+        const response = await fetch(apiUrl);
+        const post = await response.json();
+        console.log(post)
+            postBox.innerHTML += `
+            <div class="the-post-box-images-grid">
+                    <img 
+                    class="image-grid-col-two image-grid-row-two" src="/image/bali.jpg"
+                    alt="Image of a cabin on a island in bali. By the sea"
+                    data-orginal = "bali.jpg"
+                    />
+
+                    <img src="/image/diving.jpg"
+                    alt="turtle svimming in the ocean"
+                    data-orginal = "diving.jpg"
+
+                    />
+
+                    <img src="/image/food-market.jpg"
+                    alt="Foodmarket in Bali"
+                    data-orginal = "food-market.jpg"
+                    />
+
+                    <img class=" image-grid-col-two" src="/image/savanna.jpg"
+                    alt="The savanna"
+                    data-orginal = "savanna.jpg"
+                    />
+            </div>
+                <div class="page-title">
+                    <h1>${post.title.rendered}</h1>
+                </div>
+                <article>
+                            <p>${post.content.rendered}</p>
+                </article>
+                <div class="author-name">
+                    <p>x Bob</p>
+                </div>
+            </div> 
+            `
+        }
+    
+     catch{
+        console.log("error")
+     }
+    }
+
+getPost();
 
 image.forEach(image => {
     image.addEventListener("click", () => {
