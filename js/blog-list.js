6d6e2ffd9cd90a64a658c6d76dda884cfc4064df
Liisagrant <lisagrant1990@gmail.com>
2022-05-15 11:39:13 +0200

    Fixed the load more

--- js/blog-list.js
+++ js/blog-list.js
@@ -1,6 +1,9 @@
 const url =
   "https://lisagrant-943890.ingress-baronn.easywp.com/wp-json/wp/v2/posts?_embed=true&per_page=10";
+const urlLoadMore =
+  "https://lisagrant-943890.ingress-baronn.easywp.com/wp-json/wp/v2/posts?_embed=true&per_page=2&offset=10";
 const blogPostsContainer = document.querySelector(".all-blog-posts");
+const loadMore = document.querySelector("#blog-btn");
 
 const getAllBlogs = async () => {
   try {
@@ -32,9 +35,49 @@ const getAllBlogs = async () => {
       }
     }
   } catch {
-    console.log("error");
+    blogPostsContainer.innerHTML = `Sorry, we have an error`;
   } finally {
   }
 };
 
 getAllBlogs();
+
+const getMorePost = async () => {
+  try {
+    const response = await fetch(urlLoadMore);
+    const blogPosts = await response.json();
+    console.log(blogPosts);
+    for (blog of blogPosts) {
+      let data = blog._embedded["wp:featuredmedia"];
+      for (img of data) {
+        let post = `
+                    <div class="blog-post-box">
+                            <img src="${img.source_url}" alt="${img.alt_tex}">
+                        <div class="blog-post-text-box">
+                        <div class="blog-post-title">
+                            <h2>${blog.title.rendered}</h2>
+                        </div>
+                        <div class="date-post">
+                            <p>${blog.date}</p>
+                        </div>
+                        <div class="short-description">
+                            <p>${img.caption.rendered}</p>
+                        </div>
+                        <div class="read-more-link">
+                            <a href="the-post.html?id=${blog.id}""><p>Read More</p></a>
+                        </div>
+                    </div>
+                `;
+        blogPostsContainer.innerHTML += post;
+      }
+    }
+  } catch {
+    blogPostsContainer.innerHTML = `Sorry, we have an error`;
+  } finally {
+  }
+};
+
+loadMore.addEventListener("click", () => {
+  getMorePost();
+  loadMore.style.display = "none";
+});
