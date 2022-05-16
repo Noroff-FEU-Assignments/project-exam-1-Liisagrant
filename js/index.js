6597ac0e35730e0246023399e115583dc6ae9a2e
Liisagrant <lisagrant1990@gmail.com>
2022-05-16 16:08:50 +0200

    added a modal for the subscribe. still need better styling

--- js/index.js
+++ js/index.js
@@ -32,3 +32,49 @@ const getBlogPost = async () => {
 };
 
 getBlogPost();
+
+const modal = document.getElementById("modalSubscribe");
+const nameError = document.querySelector(".error-input-name");
+const emailError = document.querySelector(".error-input-email");
+const email = document.getElementById("e-mail");
+const nameInput = document.getElementById("name");
+const modalBtn = document.getElementById("modalBTN");
+
+modalBtn.onclick = (event) => {
+  event.preventDefault();
+  if (ValidEmail(email.value) === true) {
+    emailError.style.display = "none";
+  } else {
+    emailError.style.display = "block";
+  }
+
+  if (checkLength(nameInput.value, 2) === true) {
+    nameError.style.display = "none";
+  } else {
+    nameError.style.display = "block";
+  }
+
+  if (
+    checkLength(nameInput.value, 2) === true &&
+    (ValidEmail(email.value) === true) === true
+  ) {
+    modal.style.display = "block";
+  }
+};
+
+modal.onclick = () => {
+  modal.style.display = "none";
+};
+
+const checkLength = (value, len) => {
+  if (value.trim().length > len) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const ValidEmail = (email) => {
+  const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regex.test(String(email).toUpperCase());
+};
